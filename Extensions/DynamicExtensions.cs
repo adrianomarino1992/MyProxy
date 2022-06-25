6fe486351305f51e2e30052b0716b01f7b9fbdbd
Adriano <adriano@demaria.com.br>
2022-06-25 17:50:32 -0300

    before update to inject csharp code

--- Extensions/DynamicExtensions.cs
+++ Extensions/DynamicExtensions.cs
@@ -7,25 +7,33 @@ namespace MyProxy
 {
     public static class DynamicExtensions
     {
-        
 
-        public static I AddProxy<T, I>(this T @object, BeforeMethodCall before, AfterMethodCall after)
+        public static bool Call<T>(this Task<T> r)
         {
-#pragma warning disable
+            return true;
+        }
 
+        public static I CreateObjectWithProxy<T,I>(BeforeMethodCall before, AfterMethodCall after,params object[] ctorArgs) where T : I
+        {
             if (!typeof(I).IsInterface)
-                throw new InvalidTypeException(typeof(T));
+                throw new InvalidTypeException(typeof(I));
 
+
+            ConstructorInfo? ctor = MyRefs.Extensions.ReflectionExtension.GetCtorByParamsType<T>(ctorArgs.Select(s => s.GetType()).ToArray());
+            if (ctor == null)
+                throw new MyRefs.Exceptions.ContructorNotFoundException(typeof(T),$"The type {typeof(T).Name} do not have a constructor like {typeof(T).Name}({String.Join(",", ctorArgs.Select(s => s.GetType().Name).ToArray())})");
+
+#pragma warning disable
             I o = default(I);
 
             try
             {
-                o = (I)Activator.CreateInstance(new TypeGenerator(before, after).GenerateTypeFrom(typeof(T), typeof(I)));
+                o = (I)Activator.CreateInstance(new TypeGenerator(before, after).GenerateTypeFrom(typeof(T), typeof(I)), ctorArgs);
             }
             catch
             {
                 throw new FailCastException(typeof(T), typeof(I));
-            }            
+            }
 
             o.GetType().GetField("_delegate_before_call", BindingFlags.Public | BindingFlags.Instance)
              .SetValue(o, before);
@@ -33,28 +41,36 @@ namespace MyProxy
             o.GetType().GetField("_delegate_after_call", BindingFlags.Public | BindingFlags.Instance)
             .SetValue(o, after);
 
+#pragma warning restore
+            return o;
+        }
+
+        public static I AddProxy<T, I>(this T @object, BeforeMethodCall before, AfterMethodCall after) where T : I
+        {
+            I o = CreateObjectWithProxy<T,I>(before, after);
+
             @object.m_Copy(o);
 
             return o;
-#pragma warning restore
 
         }
 
         private static void m_Copy<T, I>(this T source, I target)
         {
 #pragma warning disable
-            source.GetPublicProperties(s => true).ToList().ForEach(s =>
+            source.GetAllProperties(s => true).ToList().ForEach(s =>
             {
                 object v = s.GetValue(source);
 
                 try
                 {
+
                     s.SetValue(target, v);
                 }
                 catch { }
             });
 
-            source.GetPublicFields(s => true).ToList().ForEach(s =>
+            source.GetAllFields(s => true).ToList().ForEach(s =>
             {
                 object v = s.GetValue(source);
 
